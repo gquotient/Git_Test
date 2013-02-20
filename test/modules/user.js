24c9e9168ef1ef29f44411b2508431d9b21dbac0
Justin W <justin.winslow@drakerenergy.com>
2013-02-20 17:08:04 -0600

    Added basic mocha testing

--- test/modules/user.js
+++ test/modules/user.js
@@ -1,8 +1,8 @@
 require(['../public/app/modules/user', 'jquery'], function (user, $) {
   test( "property", function() {
-    ok( user.name.length, "Passed!" );
+    ok( user.name && user.name.length, "User name property" );
   });
   test( "property", function() {
-    ok( user.get('name').length, "Passed!" );
+    ok( user.get('name') && user.get('name').length, "User get method" );
   });
 });
