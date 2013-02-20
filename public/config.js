09111909782fb2429838d73fc35292934ab6300a
jkyle <jkyle@causethought.com>
2013-02-20 10:19:15 -0600

    The Phoenix Force Rises.

---
+++ public/config.js
@@ -0,0 +1,30 @@
+// Require config
+require.config({
+  paths: {
+    // Libraries
+    jquery: 'components/jquery/jquery',
+    underscore: 'components/lodash/lodash',
+    backbone: 'components/backbone/backbone',
+    handlebars: 'components/handlebars/handlebars',
+    modernizr: 'components/modernizr/modernizr',
+    text: 'components/requirejs-text/text',
+    css: 'components/css/css'
+    // Modules
+    
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    underscore: {
+      exports: '_'
+    },
+    handlebars: {
+      exports: 'Handlebars'
+    },
+    modernizr: {
+      exports: 'Modernizr'
+    }
+  }
+});
