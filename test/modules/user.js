3d79edfe5d55fb60beed979b36f5b5c44c09d9d7
Justin W <justin.winslow@drakerenergy.com>
2013-02-21 14:25:01 -0600

    Beefier test

--- test/modules/user.js
+++ test/modules/user.js
@@ -1,9 +1,15 @@
-describe('User Model', function(){
-  describe('#properties', function(){
-    it('should have a name', function(done){
-      require(['app/modules/user'], function (user) {
-        assert(true, user.name && user.name.length);
-        assert(true, user.get('name') && user.get('name').length);
+require(['app/modules/user'], function (user) {
+  describe('User Model', function(){
+    describe('#properties', function(){
+      it('should have a name', function(done){
+        assert(user.name && user.name.length);
+        assert(user.get('name') && user.get('name').length);
+        done();
+      });
+    });
+    describe('#getMethod', function(){
+      it('method should return property', function(done){
+        assert(user.get('name') && user.get('name').length);
         done();
       });
     });
