8ffaed06987b5b6fd95ba076de4963bd38661cd1
jkyle <jkyle@causethought.com>
2013-02-21 10:43:14 -0600

    Removed unnecessary dependencies.

--- public/app/ia.js
+++ public/app/ia.js
@@ -5,13 +5,9 @@ define(
     var ia = {
       root: '/',
       views: {}
-    }; // Can we contain this without attaching to window?
+    };
 
     console.log("Hooray! We have an app.", ia);
 
-    //Compile primary templates
-    // ia.views.index = Handlebars.compile($('#index').html());
-    // ia.views.login = Handlebars.compile($('#login').html());
-
     return ia;
-})
+});
--- public/app/init.js
+++ public/app/init.js
@@ -1,27 +1,10 @@
 // Init app
 define(
   [
-      'jquery',
-      'underscore',
-      'backbone',
-      'handlebars',
-
       'app/ia',
       'app/router'
   ],
-  function ($, _, Backbone, Handlebars, ia, Router) {
-    //Create app namespace
-    //window.ia = window.ia || {};
-
-
-
-    //Replace body content with main view
-    // if (true) {
-    //   $('body').html(ia.views.index);
-    // } else {
-    //   $('body').html(ia.views.login);
-    // }
-
+  function (ia, Router) {
     ia.router = new Router();
     Backbone.history.start( {pushState: true, root: ia.root } );
   }
