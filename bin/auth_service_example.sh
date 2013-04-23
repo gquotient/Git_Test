b8668f8623341a561db93127a9f0afa3a29318bd
Ian Metcalf <ian.metcalf@drakerlabs.com>
2013-04-23 17:39:03 -0400

    Added checks for redis and postgres to auth startup script

--- bin/auth_service_example.sh
+++ bin/auth_service_example.sh
@@ -6,6 +6,23 @@ AUTH_VENV="auth"
 AUTH_TMP="/tmp/auth_service"
 AUTH_PATH="../AuthService/auth_service"
 
+redis-cli ping &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Redis is not currently running"
+  ERROR=true
+fi
+
+psql -l &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Postgresql is not currently running"
+  ERROR=true
+fi
+
+if [ $ERROR ]; then
+  echo "Cannot start auth service"
+  exit 1
+fi
+
 if [ -f $VENV_INIT ]; then
   . $VENV_INIT
   workon $AUTH_VENV
