ba10d2aa7f0475c1622740b1bea1e4e17103c36c
Ian Metcalf <ian.metcalf@drakerlabs.com>
2013-04-23 19:45:37 -0400

    Added jshint and watch to grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,26 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      files: ['public/app/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadTasks('tasks');
+
+  grunt.registerTask('dev', ['jshint', 'watch']);
 
   grunt.registerTask('default', ['serve']);
 };
