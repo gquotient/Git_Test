2fdf24911a03b93f523aea99da2c8d3a90e1f4da
Justin Winslow <justin@jwin.me>
2013-04-25 16:39:06 -0500

    Adding missing files

---
+++ public/app/modules/form/form.js
@@ -0,0 +1,30 @@
+define(
+[
+  'jquery',
+  'underscore',
+  'backbone',
+  'backbone.marionette'
+],
+function(
+  $,
+  _,
+  Backbone,
+  Marionette
+){
+
+  var DefaultForm = Marionette.ItemView.extend({
+    tagName: 'form',
+    attributes: {
+      id: 'someForm',
+      name: 'someForm'
+    },
+    events: {
+      'submit': function(event){
+        event.preventDefault();
+        console.log('form submitted', event);
+      }
+    }
+  });
+
+  return DefaultForm;
+});
