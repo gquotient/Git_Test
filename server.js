261b0783fedf88decff2d4b458ab9b3cf22c37a3
jkyle <jkyle@causethought.com>
2013-04-26 10:55:59 -0500

    Pulled routes into their own file. Might pull out more later.

--- server.js
+++ server.js
@@ -13,53 +13,8 @@ var express = require('express')
   , flash = require('connect-flash')
   , DrakerIA6Strategy = require('./lib/strategies/passport-draker-ia6').Strategy
   , fs = require('fs')
-  , hbs = require('hbs');
-
-/**
- * , LocalStrategy = require('passport-local').Strategy
- */
-
-var port = process.env.PORT || 3005;
-
-/**
- * Stub out some users.
- */
-
-var User = {
-  users: [
-  ],
-  findBy: function(field, value, callback){
-    var currentuser = _.find(this.users, function(user){
-      return user[field] === value;
-    });
-
-    if( currentuser ){
-      callback(null, currentuser);
-    } else {
-      callback(null, false, { message: "User not found." });
-    }
-  }
-};
-
-/**
- * Stub out some random data.
- */
-
-var Projects = {
-  projects: [
-    {name: "Foo", color: "blue", id: 1},
-    {name: "Bar", color: "red", id: 2}
-  ],
-  findBy: function(field, value, callback){
-    var currentproject = _.find(this.projects, function(project){
-      return project[field] === value;
-    });
-
-    if(currentproject){
-      callback(null, currentproject);
-    }
-  }
-};
+  , hbs = require('hbs')
+  , routes = require('./routes');
 
 /*
  * Configure Express App
@@ -103,7 +58,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
   app.set('clientID', 'IA6_0.1');
   app.set('clientSecret', 'ed75d8d3a96ef67041b52e057a5c86c3');
-  app.set('callbackURL', 'http://127.0.0.1:' + port + '/token');
+  app.set('callbackURL', 'http://127.0.0.1:' + app.get('port') + '/token');
   app.set('authPort', 8431);
   app.set('authUrl', '127.0.0.1');
 });
@@ -113,7 +68,7 @@ app.configure('development-remote', function(){
   app.use(express.errorHandler());
   app.set('clientID', 'IA6_0.1');
   app.set('clientSecret', 'ed75d8d3a96ef67041b52e057a5c86c3');
-  app.set('callbackURL', 'http://127.0.0.1:' + port + '/token');
+  app.set('callbackURL', 'http://127.0.0.1:' + app.get('port') + '/token');
   app.set('authorizationURL', 'http://auth.stage.intelligentarray.com/ia/oauth2/auth');
   app.set('tokenURL', 'http://auth.stage.intelligentarray.com/ia/oauth2/token');
   app.set('authPort', 80);
@@ -147,175 +102,16 @@ passport.deserializeUser(function(user, done) {
 });
 
 /*
-  Register partials
-*/
+ * Register partials
+ */
 
 var headerTemplate = fs.readFileSync(__dirname + '/templates/partials/header.hbs', 'utf8');
 hbs.registerPartial('sharedHeader', headerTemplate);
 
 /*
- * Basic routing (temporary).
+ * Setup Routes
  */
-
-app.all('/', ensureAuthenticated, function(req, res){
-  res.redirect('/ia');
-});
-
-app.all('/ia', ensureAuthenticated, function(req, res){
-  var portfolios, projects;
-  // Load portfolios.
-  fs.readFile('./data/json/portfolios.json', 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      portfolios = data;
-
-      // Load projects.
-      fs.readFile('./data/json/projects.json', 'utf8', function (err, data) {
-        if (err) {
-          return console.log(err);
-        }
-        projects = data;
-
-        // Render the response.
-        res.render(
-          'index',
-          {
-            user: JSON.stringify({
-              username: req.user.name,
-              firstName: req.user.name.split(' ')[0],
-              lastName: req.user.name.split(' ')[1],
-              email: req.user.email
-            }),
-            portfolios: portfolios,
-            projects: projects,
-            locale: (req.user.locale) ? req.user.locale : req.acceptedLanguages[0]
-          }
-        );
-      });
-    });
-});
-
-app.all('/ia/*', ensureAuthenticated, function(req, res){
-  var newUrl = req.url.split('/').slice(2).join('/');
-  res.redirect( req.protocol + '://' + req.get('Host') + '/ia/#/' + newUrl );
-});
-
-
-app.get('/login', function(req, res){
-  if (req.isAuthenticated() ) {
-    if (req.session.redirectUrl) {
-      res.redirect(req.session.redirectUrl);
-    } else {
-      res.redirect('/ia');
-    }
-  } else {
-    res.render('login', { flash: req.flash('error') });
-  }
-});
-
-app.post('/login',
-  passport.authenticate('draker-ia6',
-  {
-    successRedirect: '/reset',
-    failureRedirect: '/login',
-    failureFlash: true
-  }
-));
-
-/* Reset Password */
-app.get('/reset', ensureAuthenticated, function(req, res){
-  if( req.user.firsttime ){
-    res.render('resetpassword', { flash: req.flash('error') });
-  } else {
-      if (req.session.redirectUrl) {
-        res.redirect(req.session.redirectUrl);
-      } else {
-        res.redirect('/ia');
-      }
-  }
-});
-
-app.post('/reset', function(req, res){
-  // console.log( 'passwords:' + req.body.password + ':' + req.body.password_new + ':' + req.body.password_check );
-  if (req.body.password_new === req.body.password_check) {
-    if (req.body.password_new.length > 5) {
-      console.log( 'post the password reset request' );
-      DrakerIA6Strategy.reset(req, res, app, function(req, res, post_res) {
-        console.log( "Back from reset:" + post_res.status );
-        if ( post_res.status === 200 ) {
-          res.redirect('/ia');
-        } else {
-          if ( post_res.message ) {
-            req.flash( 'error', post_res.message );
-          } else {
-            req.flash( 'error', 'Unauthorized' );
-          }
-          res.redirect('/reset');
-        }
-
-      });
-    } else {
-      req.flash( 'error', 'New password is too short');
-      res.redirect('/reset');
-    }
-  } else {
-    req.flash( 'error', 'New passwords did not match');
-    res.redirect('/reset');
-  }
-});
-
-app.get('/token',
-  passport.authenticate('draker-ia6', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res){
-    req.session["draker-ia6"] = req.session["passport"]["user"];
-    /* res.render("index",checkSession(req)); */
-    res.redirect('/reset');
-  }
-);
-
-/* Logout */
-app.get('/logout',
-  function(req, res){
-    DrakerIA6Strategy.logout(req, res, app, function(req, res, post_res) {
-      // console.log( "Back from logout:" + post_res.status );
-    });
-    // req.logout();
-    req.session.destroy();
-    res.redirect('/login');
-  }
-);
-
-/* API */
-app.all('/api/*', ensureAuthenticated);
-
-app.get('/api/portfolios',
-  function(req, res){
-    fs.readFile('./data/json/portfolios.json', 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      res.end(data);
-    });
-  });
-
-app.get('/api/projects',
-  function(req, res){
-    fs.readFile('./data/json/projects.json', 'utf8', function (err, data){
-      if (err) {
-        return console.log(err);
-      }
-      res.end(data);
-    });
-  });
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  else {
-    if(req.url !== '/reset' || req.url !== '/login') { req.session.redirectUrl = req.url; }
-    res.redirect('/login');
-  }
-}
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
