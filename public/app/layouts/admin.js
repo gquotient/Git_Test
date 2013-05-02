4934c466152791cff91e139793cd87c4e4e93100
Justin Winslow <justin@jwin.me>
2013-05-02 02:07:40 -0500

    Fixed issue where changing views would kill event handlers

--- public/app/layouts/admin.js
+++ public/app/layouts/admin.js
@@ -39,37 +39,54 @@ define([
 
     initialView: 'users',
 
-    subViews: {
-      users: {
-        title: 'Edit Users'
-      },
-      teams: {
-        title: 'Edit Teams'
-      },
-      organizations: {
-        title: 'Edit Organizations'
-      }
+    // Sub Views
+    users: function(){
+      var collection = new User.Collection(),
+          view = new User.views.editTable({ collection: collection });
+
+      // Fetch latest
+      collection.fetch();
+
+      // Update page title
+      this.$el.find('.pageTitle').text('Edit Users');
+
+      return view;
+    },
+    teams: function(){
+      // Buid teams edit view
+      var collection = new Team.collections.Teams(),
+          view = new Team.views.editTable({ collection: collection });
+
+      // Fetch latest
+      collection.fetch();
+
+      // Update page title
+      this.$el.find('.pageTitle').text('Edit Teams');
+
+      return view;
+    },
+    organizations: function(){
+      // Buid organizations edit view
+      var collection = new Organization.collections.Organizations(),
+          view = new Organization.views.editTable({ collection: collection });
+
+      collection.fetch();
+
+      // Update page title
+      this.$el.find('.pageTitle').text('Edit Organizations');
+
+      return view;
     },
 
     renderView: function(view){
-      var myView = this.subViews[view];
+      var myView = this[view]();
 
       // Set active nav element
       this.$el.find('.nav_content li').removeClass('active');
       this.$el.find('.nav_content li.' + view).addClass('active');
 
-      // Update page title
-      this.$el.find('.pageTitle').text(myView.title);
-
-      // Fetch latest data
-      myView.view.collection.fetch();
-
       // Display view
-      this.pageContent.show(myView.view);
-
-      // Fetch latest
-      // NOTE: this is mostly a hack to avoid the itemViewError in Marionette
-      myView.view.collection.fetch();
+      this.pageContent.show(myView);
     },
 
     onShow: function(){
@@ -97,24 +114,6 @@ define([
       if (options.initialView) {
         this.initialView = options.initialView;
       }
-
-      // Buid users edit view
-      var users = new User.Collection();
-      //users.fetch();
-
-      this.subViews.users.view = new User.views.editTable({ collection: users });
-
-      // Buid teams edit view
-      var teams = new Team.collections.Teams();
-      //teams.fetch();
-
-      this.subViews.teams.view = new Team.views.editTable({ collection: teams });
-
-      // Buid organizations edit view
-      var organizations = new Organization.collections.Organizations();
-      //organizations.fetch();
-
-      this.subViews.organizations.view = new Organization.views.editTable({ collection: organizations });
     }
   });
 });
