49c8ef22028528c7d72607f39ab45461a1155c61
Justin Winslow <justin@jwin.me>
2013-06-20 00:13:35 -0500

    improving container css

--- public/app/modules/project/project.js
+++ public/app/modules/project/project.js
@@ -209,7 +209,7 @@ define([
     },
 
     render: function(){
-      console.log(this.model);
+
       var that = this,
         popUpContent = _.template([
           '<h4><%= display_name %></h4>' +
