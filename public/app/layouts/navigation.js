a0a2a938c10efa64565171de3e897dc4543f5e8e
jkyle <jkyle@causethought.com>
2013-06-26 16:06:18 -0500

    Made better breadcrumb support for back-button. Added reset method.

--- public/app/layouts/navigation.js
+++ public/app/layouts/navigation.js
@@ -25,22 +25,37 @@ define([
       crumbs: '#breadcrumbs'
     },
 
+    setBreadcrumbs: function(model){
+      var breadcrumbModel = new Breadcrumb.Model(model);
+
+      if(this.app.state !== breadcrumbModel.get('state')){
+        this.breadcrumbs.advance(breadcrumbModel);
+        this.app.state = breadcrumbModel.get('state');
+      } else {
+        this.breadcrumbs.update(breadcrumbModel);
+      }
+    },
+
+    resetBreadcrumbs: function(model){
+      var breadcrumbModel = new Breadcrumb.Model(model);
+
+      this.breadcrumbs.reset([breadcrumbModel]);
+      this.app.state = breadcrumbModel.get('state');
+    },
+
     initialize: function(options){
       this.app = options.app;
 
-      var breadcrumbs = new Breadcrumb.Collection();
-      this.breadcrumbsView = new Breadcrumb.views.Breadcrumbs({collection: breadcrumbs});
+      this.breadcrumbs = new Breadcrumb.Collection();
+      this.breadcrumbsView = new Breadcrumb.views.Breadcrumbs({collection: this.breadcrumbs});
 
       this.listenTo(Backbone, 'set:breadcrumbs', function(model){
-        var breadcrumbModel = new Breadcrumb.Model(model);
-
-        if(this.app.state !== breadcrumbModel.get('state')){
-          breadcrumbs.advance(breadcrumbModel);
-          this.app.state = breadcrumbModel.get('state');
-        } else {
-          breadcrumbs.update(breadcrumbModel);
-        }
+        this.setBreadcrumbs(model);
       }, this);
+
+      this.listenTo(Backbone, 'reset:breadcrumbs', function(model){
+        this.resetBreadcrumbs(model);
+      })
 
     },
 
