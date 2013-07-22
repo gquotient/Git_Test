dccedde0cd7cc3c257d4f18cf2397e7b798d4677
Justin Winslow <justin@jwin.me>
2013-07-21 22:50:09 -0500

    Adding dynamic trace selection

--- public/app/layouts/devices/inverter.js
+++ public/app/layouts/devices/inverter.js
@@ -25,21 +25,41 @@ define([
     buildCharts: function(){
       var
         that = this,
-        project = this.options.project
+        project = this.options.project,
+        traces = [],
+        series = []
       ;
 
       project.findDataSources().done(function(dataSources){
+        // Add irradiance trace
+        traces.push(Chart.dataDefaults(project, that.model, 'irradiance'));
+        series.push(Chart.seriesDefaults.irradiance);
+
+        // Selectively populate power traces
+        if (dataSources.inverter.ac_power) {
+          traces.push({
+            'project_label': project.id,
+            'ddl': 'inv',
+            'dtstart': 'today',
+            'dtstop': 'now',
+            'columns': ['freezetime', 'ac_power'],
+            'filters': [
+              {
+                'column': 'identifier',
+                'in_set': [that.model.get('graph_key')]
+              }
+            ],
+            project_timezone: project.timezone || null
+          });
+
+          series.push(Chart.seriesDefaults.power);
+        }
+
         var chart_powerAndIrradiance = new Chart.views.Line({
           model: new Chart.models.timeSeries().set({
-            'traces': [
-              Chart.dataDefaults(project, that.model, 'irradiance'),
-              Chart.dataDefaults(project, that.model, 'power')
-            ]
+            'traces': traces
           }),
-          series: [
-            Chart.seriesDefaults.irradiance,
-            Chart.seriesDefaults.power
-          ]
+          series: series
         });
 
         var chart_currentAndVoltage = new Chart.views.Line({
